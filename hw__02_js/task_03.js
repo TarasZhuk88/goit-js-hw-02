1c3feedffb0f636ff4cc41b39bf89328e203cff3
Taras Zhuk <66519608+TarasZhuk88@users.noreply.github.com>
2020-09-27 01:23:05 +0300

    task_01_02_03

---
+++ hw__02_js/task_03.js
@@ -0,0 +1,23 @@
+function findLongestWord (string = "") {
+    // Write code under this line
+    let str = string.split(" ")
+    let longest = 0;
+    let word = "";
+    for (let i = 0; i < str.length - 1; i++) {
+        if (longest < str[i].length) {
+        longest = str[i].length;
+        word = str[i];
+        }
+    }
+    return word;
+}
+
+
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
+'jumped'
+
+console.log(findLongestWord('Google do a roll'));
+'Google'
+
+console.log(findLongestWord('May the force be with you'));
+'force'
